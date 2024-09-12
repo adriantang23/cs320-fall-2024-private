de3ae68deebc42301bf80fb97684126e3cd5b73f
Adrian Tang <adriantang@crc-dot1x-nat-10-239-229-122.bu.edu>
2024-09-12 19:12:08 -0400

    changed starting to 0 instead of 1, resetting to 1 causes an infinite loop

--- assigns/assign01/lib/assign01_04.ml
+++ assigns/assign01/lib/assign01_04.ml
@@ -10,7 +10,7 @@ let to_string n =
                 if n mod p = 0 then (p, i)
                 else prime (i + 1)
             in
-            let (p,i) = prime 1 in
+            let (p,i) = prime 0 in
             let format = 
                 if first then "[" ^ string_of_int i 
                 else "; " ^ string_of_int i in format ^ build (n/p) false
